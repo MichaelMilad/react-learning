f8f41a931bc20b4a2a2c7f9a8f6ef6ca82ce044b
Michael Milad <michaelmilad001@gmail.com>
2026-07-19 18:22:32 +0300

    updates

---
+++ usepopcorn/src/components/Main.js
@@ -0,0 +1,155 @@
+import { useState } from 'react';
+
+const tempWatchedData = [
+	{
+		imdbID: 'tt1375666',
+		Title: 'Inception',
+		Year: '2010',
+		Poster:
+			'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+		runtime: 148,
+		imdbRating: 8.8,
+		userRating: 10,
+	},
+	{
+		imdbID: 'tt0088763',
+		Title: 'Back to the Future',
+		Year: '1985',
+		Poster:
+			'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+		runtime: 116,
+		imdbRating: 8.5,
+		userRating: 9,
+	},
+];
+
+const average = (arr) =>
+	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+function ListBox({ movies }) {
+	const [isOpen, setIsOpen] = useState(true);
+
+	return (
+		<div className='box'>
+			<button className='btn-toggle' onClick={() => setIsOpen((open) => !open)}>
+				{isOpen ? '–' : '+'}
+			</button>
+			{isOpen && <MovieList movies={movies} />}
+		</div>
+	);
+}
+
+function MovieList({ movies }) {
+	return (
+		<ul className='list'>
+			{movies?.map((movie) => (
+				<Movie key={movie.imdbID} movie={movie} />
+			))}
+		</ul>
+	);
+}
+
+function Movie({ movie }) {
+	return (
+		<li>
+			<img src={movie.Poster} alt={`${movie.Title} poster`} />
+			<h3>{movie.Title}</h3>
+			<div>
+				<p>
+					<span>🗓</span>
+					<span>{movie.Year}</span>
+				</p>
+			</div>
+		</li>
+	);
+}
+
+function WatchedBox() {
+	const [watched, setWatched] = useState(tempWatchedData);
+	const [isOpen, setIsOpen] = useState(true);
+
+	return (
+		<div className='box'>
+			<button className='btn-toggle' onClick={() => setIsOpen((open) => !open)}>
+				{isOpen ? '–' : '+'}
+			</button>
+			{isOpen && (
+				<>
+					<WatchedSummary watched={watched} />
+					<WatchedMovieList watched={watched} />
+				</>
+			)}
+		</div>
+	);
+}
+
+function WatchedMovieList({ watched }) {
+	return (
+		<ul className='list'>
+			{watched.map((movie) => (
+				<WatchedMovie key={movie.imdbID} movie={movie} />
+			))}
+		</ul>
+	);
+}
+
+function WatchedSummary({ watched }) {
+	const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+	const avgUserRating = average(watched.map((movie) => movie.userRating));
+	const avgRuntime = average(watched.map((movie) => movie.runtime));
+	return (
+		<div className='summary'>
+			<h2>Movies you watched</h2>
+			<div>
+				<p>
+					<span>#️⃣</span>
+					<span>{watched.length} movies</span>
+				</p>
+				<p>
+					<span>⭐️</span>
+					<span>{avgImdbRating}</span>
+				</p>
+				<p>
+					<span>🌟</span>
+					<span>{avgUserRating}</span>
+				</p>
+				<p>
+					<span>⏳</span>
+					<span>{avgRuntime} min</span>
+				</p>
+			</div>
+		</div>
+	);
+}
+
+function WatchedMovie({ movie }) {
+	return (
+		<li>
+			<img src={movie.Poster} alt={`${movie.Title} poster`} />
+			<h3>{movie.Title}</h3>
+			<div>
+				<p>
+					<span>⭐️</span>
+					<span>{movie.imdbRating}</span>
+				</p>
+				<p>
+					<span>🌟</span>
+					<span>{movie.userRating}</span>
+				</p>
+				<p>
+					<span>⏳</span>
+					<span>{movie.runtime} min</span>
+				</p>
+			</div>
+		</li>
+	);
+}
+
+export default function Main({ movies }) {
+	return (
+		<main className='main'>
+			<ListBox movies={movies} />
+			<WatchedBox movies={movies} />
+		</main>
+	);
+}
